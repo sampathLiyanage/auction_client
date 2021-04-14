02586fa224d0cedeae4af935f13db7a5226a813c
Sampath Liyanage <plbsam@gmail.com>
2021-04-14 12:22:04 +0530

    adding routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ItemListComponent } from './item-list/item-list.component';
+import { BiddingComponent } from './bidding/bidding.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ItemListComponent},
+  {path: 'items/:id', component: BiddingComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
